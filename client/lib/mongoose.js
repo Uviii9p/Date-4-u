6690d953e2a7df76f341b2e3da7a6524746b0a2f
uviiiiiiii <invincible070007@gmail.com>
2026-02-17 18:05:57 +0530

    Vercel Fix: Re-enable bufferCommands for more stable serverless connections

--- client/lib/mongoose.js
+++ client/lib/mongoose.js
@@ -19,7 +19,8 @@ async function dbConnect() {
 
     if (!cached.promise) {
         const opts = {
-            bufferCommands: false,
+            bufferCommands: true,
+            serverSelectionTimeoutMS: 5000,
         };
 
         cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
